813eba0027d6b25e294ee84df08f10e8f161b199
DuchLord <duchon.ondrej@gmail.com>
2016-09-09 23:45:10 +0200

    Command info

--- src/main/java/treechopper/common/command/Commands.java
+++ src/main/java/treechopper/common/command/Commands.java
@@ -21,74 +21,123 @@ import java.util.List;
 
 public class Commands extends CommandBase {
 
+    @Override
     public String getCommandName() {
         return "treechop";
     }
 
+    @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "/treechop <ignoreDur|plantSap|decayLeaves|breakSpeed> value";
+        return "/treechop <info|printname|ignoreDur|plantSap|decayLeaves|breakSpeed> value";
     }
 
-    public int getRequiredPermissionLevel() {
-        return 0;
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
     }
 
+    @Override
     public List getCommandAliases() {
         return Lists.newArrayList("tch");
     }
 
+    @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        if (args.length < 1)
-            throw new WrongUsageException("/treechop <ignoredur|plantsap|decayleaves|breakspeed> [value]");
 
-        else if (args[0].equals("ignoredur")) {
-            if (args.length != 2)
-                throw new WrongUsageException("/treechop ignoredur [0,1]");
+        if (sender.canCommandSenderUseCommand(2, this.getCommandName())) {
+            if (args.length < 1)
+                throw new WrongUsageException("/treechop <info|printname|ignoredur|plantsap|decayleaves|breakspeed> [value]");
 
-            ConfigHandler.setIgnoreDurability(parseBoolean(args[1]));
-            if (ConfigHandler.ignoreDurability)
-                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Ignore axe durability has been switched " + TextFormatting.GREEN + "ON"));
-            else
-                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Ignore axe durability has been switched " + TextFormatting.RED + "OFF"));
+            else if (args[0].equals("ignoredur")) {
+                if (args.length != 2)
+                    throw new WrongUsageException("/treechop ignoredur [0,1]");
 
-        } else if (args[0].equals("plantsap")) {
-            if (args.length != 2)
-                throw new WrongUsageException("/treechop plantsap [0,1]");
+                ConfigHandler.setIgnoreDurability(parseBoolean(args[1]));
+                if (ConfigHandler.ignoreDurability)
+                    sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Ignore axe durability has been switched " + TextFormatting.GREEN + "ON"));
+                else
+                    sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Ignore axe durability has been switched " + TextFormatting.RED + "OFF"));
 
-            ConfigHandler.setPlantSapling(parseBoolean(args[1]));
-            if (ConfigHandler.plantSapling)
-                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Auto sapling has been switched " + TextFormatting.GREEN + "ON"));
-            else
-                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Auto sapling has been switched " + TextFormatting.RED + "OFF"));
+            } else if (args[0].equals("plantsap")) {
+                if (args.length != 2)
+                    throw new WrongUsageException("/treechop plantsap [0,1]");
 
-        } else if (args[0].equals("decayleaves")) {
-            if (args.length != 2)
-                throw new WrongUsageException("/treechop decayleaves [0,1]");
+                ConfigHandler.setPlantSapling(parseBoolean(args[1]));
+                if (ConfigHandler.plantSapling)
+                    sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Auto sapling has been switched " + TextFormatting.GREEN + "ON"));
+                else
+                    sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Auto sapling has been switched " + TextFormatting.RED + "OFF"));
 
-            ConfigHandler.setDecayLeaves(parseBoolean(args[1]));
-            sender.addChatMessage(new TextComponentTranslation("Decay leaves set to " + ConfigHandler.decayLeaves));
-            if (ConfigHandler.decayLeaves)
-                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Decay leaves has been switched " + TextFormatting.GREEN + "ON"));
-            else
-                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Decay leaves has been switched " + TextFormatting.RED + "OFF"));
+            } else if (args[0].equals("decayleaves")) {
+                if (args.length != 2)
+                    throw new WrongUsageException("/treechop decayleaves [0,1]");
 
-        } else if (args[0].equals("breakspeed")) {
-            if (args.length != 2)
-                throw new WrongUsageException("/treechop breakspeed [value]");
+                ConfigHandler.setDecayLeaves(parseBoolean(args[1]));
+                if (ConfigHandler.decayLeaves)
+                    sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Decay leaves has been switched " + TextFormatting.GREEN + "ON"));
+                else
+                    sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Decay leaves has been switched " + TextFormatting.RED + "OFF"));
 
-            ConfigHandler.setBreakSpeed(parseInt(args[1], 1, 1000));
-            sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Break speed has been set to " + ConfigHandler.breakSpeed + ", [DEFAULT: 10]"));
+            } else if (args[0].equals("breakspeed")) {
+                if (args.length != 2)
+                    throw new WrongUsageException("/treechop breakspeed [value]");
 
-            TreeChopper.network.sendToAll(new ServerMessage(ConfigHandler.breakSpeed));
-        } else
-            throw new WrongUsageException("/treechop <ignoredur|plantsap|decayleaves|breakspeed> [value]");
+                ConfigHandler.setBreakSpeed(parseInt(args[1], 1, 1000));
+                sender.addChatMessage(new TextComponentTranslation("[" + TextFormatting.GOLD + "TCH" + TextFormatting.RESET + "]Break speed has been set to " + ConfigHandler.breakSpeed + ", [DEFAULT: 10]"));
+
+                TreeChopper.network.sendToAll(new ServerMessage(ConfigHandler.breakSpeed));
+
+            } else if (args[0].equals("info") || args[0].equals("?")) {
+                printSettings(sender);
+
+            } else if (args[0].equals("printname")) {
+
+            } else
+                throw new WrongUsageException("/treechop <info|printname|ignoredur|plantsap|decayleaves|breakspeed> [value]");
+        } else {
+            if (args.length < 1)
+                throw new WrongUsageException("/treechop <info|printname>");
+
+            else if (args[0].equals("info") || args[0].equals("?")) {
+                printSettings(sender);
+
+            } else if (args[0].equals("printname")) {
+
+            } else
+                throw new WrongUsageException("/treechop <info|printname>");
+        }
+
+        if (args[0].equals("printname")) {
+            // TODO Printing item name
+        }
 
         ConfigHandler.writeConfig(ConfigHandler.decayLeaves, ConfigHandler.plantSapling, ConfigHandler.ignoreDurability, ConfigHandler.breakSpeed);
     }
 
+    private void printSettings(ICommandSender sender) {
+        sender.addChatMessage(new TextComponentTranslation(TextFormatting.GOLD + "Tree Chopper" + TextFormatting.RESET + " settings: \n--------------------"));
+        sender.addChatMessage(new TextComponentTranslation("Break speed: " + TextFormatting.GOLD + ConfigHandler.breakSpeed));
+
+        if (ConfigHandler.decayLeaves)
+            sender.addChatMessage(new TextComponentTranslation("Leaves decay: " + TextFormatting.GREEN + "ON"));
+        else
+            sender.addChatMessage(new TextComponentTranslation("Leaves decay: " + TextFormatting.RED + "OFF"));
+
+        if (ConfigHandler.plantSapling)
+            sender.addChatMessage(new TextComponentTranslation("Automatic sapling plant: " + TextFormatting.GREEN + "ON"));
+        else
+            sender.addChatMessage(new TextComponentTranslation("Automatic sapling plant: " + TextFormatting.RED + "OFF"));
+
+        if (ConfigHandler.ignoreDurability)
+            sender.addChatMessage(new TextComponentTranslation("Ignore durability: " + TextFormatting.GREEN + "ON"));
+        else
+            sender.addChatMessage(new TextComponentTranslation("Ignore durability: " + TextFormatting.RED + "OFF"));
+    }
+
+    @Override
     public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos pos) {
         if (args.length == 1)
-            return getListOfStringsMatchingLastWord(args, "ignoredur", "plantsap", "decayleaves", "breakspeed");
+            return getListOfStringsMatchingLastWord(args, "ignoredur", "plantsap", "decayleaves", "breakspeed", "info", "printname");
 
         return null;
     }
